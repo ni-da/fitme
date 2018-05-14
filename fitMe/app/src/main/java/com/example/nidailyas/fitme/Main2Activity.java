f4620f89d4208e4a2310138e373f50c3dcbfb6f7
Nida Ilyas <38118425+ni-da@users.noreply.github.com>
2018-05-14 13:46:28 +0200

    refactoring

--- fitMe/app/src/main/java/com/example/nidailyas/fitme/Main2Activity.java
+++ fitMe/app/src/main/java/com/example/nidailyas/fitme/Main2Activity.java
@@ -9,28 +9,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
 import java.util.ArrayList;
 
 public class Main2Activity extends AppCompatActivity {
-    LinearLayoutManager layoutManager;
-    DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
-    //ArrayList<Pair> items = new ArrayList<>();
-    ArrayList<Habit> habits = new ArrayList<>();
-    ArrayList<String> habitIds = new ArrayList<>();
-
-    ViewGroup activity_main2;
-    TextView circle_level;
-    TextView textView_signatureName;
-    TextView textView_gold_coins_count;
-    RecyclerView tasksList;
-    TextView textView_gold_coins;
+    private LinearLayoutManager layoutManager;
+    private ArrayList<String> habitIdss = new ArrayList<>();
+    private ViewGroup activity_main2;
+    private TextView circle_level;
+    private TextView textView_signatureName;
+    private TextView textView_gold_coins_count;
+    private RecyclerView tasksList;
+    private TextView textView_gold_coins;
 
 
     @Override
@@ -39,17 +28,25 @@ public class Main2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
         activity_main2 = findViewById(R.id.activity_main2);
         tasksList = (RecyclerView) findViewById(R.id.tasks_list);
-        layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,
+                false);
         tasksList.setLayoutManager(layoutManager);
-        tasksList.addOnItemTouchListener(new RecyclerItemClickListener(Main2Activity.this, new RecyclerItemClickListener.OnItemClickListener() {
-            @Override
-            public void onItemClick(View view, int position) {
-                Intent intent = new Intent(getApplicationContext(), ExecuteHabitActivity.class);
-                intent.putExtra("habitId", habitIds.get(position));
 
-                startActivity(intent);
-            }
-        }));
+        tasksList.addOnItemTouchListener(new RecyclerItemClickListener
+                (Main2Activity.this,
+                        new RecyclerItemClickListener.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(View view, int position) {
+                                Intent intent = new Intent(getApplicationContext(),
+                                        ExecuteHabitActivity.class);
+                                intent.putExtra("habitId",
+                                        habitIdss.get(position));
+
+                                startActivity(intent);
+                            }
+                        }));
+
+
         showHabits(tasksList);
 
         findViewById(R.id.imageView_profile_pic).setOnClickListener(new View.OnClickListener() {
@@ -112,71 +109,17 @@ public class Main2Activity extends AppCompatActivity {
 
 
     private void showHabits(final RecyclerView tasksList) {
-//        new HabitManager().getUserHabits(new MyCallback<ArrayList<Habit>>() {
-//            @Override
-//            public void onCallback(ArrayList<Habit> habits) {
-//                tasksList.setAdapter(new taskAdapter(habits));
-//                for (Habit habit : habits) {
-//                    habitIds.add(habit.getHabitId());
-//                }
-//            }
-//        });
-
-        String userId = new UserManager().getCurrentUserIdFromDb();
-        // Toast.makeText(Main2Activity.this, "Userid: " + userId, Toast.LENGTH_SHORT).show();
-        mDatabase.child("users").child(FirebaseAuth.getInstance().getCurrentUser().getUid())
-                .addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        if (dataSnapshot.exists()) {
-                            User user = dataSnapshot.getValue(User.class);
-                            String planningId = user.planningId;
-                            mDatabase.child("plannings").child(planningId).addListenerForSingleValueEvent(new ValueEventListener() {
-                                @Override
-                                public void onDataChange(DataSnapshot dataSnapshot) {
-                                    Planning planning = dataSnapshot.getValue(Planning.class);
-                                    final ArrayList<String> habitsFreqIds = planning.getHabitFrequencies();
-                                    for (String object : habitsFreqIds) {
-                                        mDatabase.child("habitFrequencyTimings").child(object).addListenerForSingleValueEvent(new ValueEventListener() {
-                                            @Override
-                                            public void onDataChange(DataSnapshot dataSnapshot) {
-                                                HabitFrequencyTiming habitFrequencyTiming = dataSnapshot.getValue(HabitFrequencyTiming.class);
-
-                                                mDatabase.child("habits").child(habitFrequencyTiming.getHabitId()).addListenerForSingleValueEvent(new ValueEventListener() {
-                                                    @Override
-                                                    public void onDataChange(DataSnapshot dataSnapshot) {
-                                                        Habit habit = dataSnapshot.getValue(Habit.class);
-                                                        habitIds.add(habit.getHabitId());
-                                                        habits.add(habit);
-                                                        tasksList.setAdapter(new taskAdapter(habits));
-
-                                                    }
-
-                                                    @Override
-                                                    public void onCancelled(DatabaseError databaseError) {
-                                                    }
-                                                });
-                                            }
-
-                                            @Override
-                                            public void onCancelled(DatabaseError databaseError) {
-                                            }
-                                        });
-                                    }
-                                }
-
-                                @Override
-                                public void onCancelled(DatabaseError databaseError) {
-                                }
-                            });
-                        }
+        new HabitManager().getUserHabits(new MyCallback<ArrayList<Habit>>() {
+            @Override
+            public void onCallback(ArrayList<Habit> habits) {
+                tasksList.setAdapter(new taskAdapter(habits));
+                for (Habit habit : habits) {
+                    if (!habitIdss.contains(habit.getHabitId())) {
+                        habitIdss.add(habit.getHabitId());
                     }
 
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-                    }
-                });
+                }
+            }
+        });
     }
-
-
 }
